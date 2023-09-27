d1fd59790617aeebca2dc2dd1d6068bb8e778bc7
farzanfqb <farzanfqb@gmail.com>
2023-09-27 23:05:27 +0530

    tried to make AddService using spring core java based config class but navigation is failing in path finding

--- src/main/java/spring/AddController.java
+++ src/main/java/spring/AddController.java
@@ -1,19 +1,22 @@
 package spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import spring.service.AddService;
+import spring.service.AppConfig;
 
 @Controller
 public class AddController {
 	@RequestMapping("add")
 	public ModelAndView add(@RequestParam("t1") int num1, @RequestParam("t2") int num2){
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		ModelAndView mv = new ModelAndView();
-		AddService as = new AddService();
-		mv.addObject("result",as.add(num1, num2) );
+		//AddService as = new AddService();
+		mv.addObject("result",((AddService) applicationContext).add(num1, num2) );
 		mv.setViewName("result.jsp");
 		return mv;
 	}
--- src/main/java/spring/service/AddService.java
+++ src/main/java/spring/service/AddService.java
@@ -1,5 +1,9 @@
 package spring.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class AddService {
 public int add(int num1, int num2) {
 	int result = num1+num2;
